ba4f335531d5a0dc9f9d466ab14723af40284ef7
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-11-08 01:33:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,34 @@
- let banse = new Set();
+ 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`Start ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+	
+});
+
+let banse = new Set();
   client.on('guildBanAdd', function(guild) {
     guild.fetchAuditLogs().then(logs => {
       const ser = logs.entries.first().executor;
